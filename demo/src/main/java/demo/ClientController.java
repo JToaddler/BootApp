3ab098abef6a55dddae01cbe737d5362a15c4d53
JToaddler <javasmoker@gmail.com>
2015-04-16 12:18:57 -0700

    initial

--- demo/src/main/java/demo/ClientController.java
+++ demo/src/main/java/demo/ClientController.java
@@ -1,16 +1,42 @@
 package demo;
 
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HelloWorldController {
+public class ClientController {
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public ModelAndView hello() {
-		return new ModelAndView("hello").addObject("name", "Yashwant");
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public ModelAndView home() {
+		return new ModelAndView("home");
 	}
+	
+	
+	public String formatXML()
+	{
+		
+		String xml = "<note><to>Tove</to><from>Jani</from><heading><heading>Reminder</heading></heading><body>Don't forget me this weekend!</body></note>";
+		final StringWriter sw  = new StringWriter();
+	    try {
+			final OutputFormat format = OutputFormat.createPrettyPrint();
+			final org.dom4j.Document document = DocumentHelper.parseText(xml);
+	        final XMLWriter writer = new XMLWriter(sw, format);
+	        writer.write(document);
+		} catch (DocumentException |IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return sw.toString();
+	}
+	
 
 }
